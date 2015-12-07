a61725c689e37ee1f0d2a69244bc9a1952bd34e2
ZachariasOctavious at codea-scm <ZachariasOctavious@codea-scm.aws.mapote.com>
2015-12-06 20:04:23 -0500

    added if statements in the main game scene

--- tabs/MainGameScene.lua
+++ tabs/MainGameScene.lua
@@ -2,17 +2,83 @@ MainGameScene = class()
 
 function MainGameScene:init()
     -- you can accept and set parameters here
+    
+    --test to see if the worldSelected and levelDifficultySelected variables work for these if statements in the main game
+    
+    --addition world
+    if( (worldSelected == "ADDITION") and
+        (levelDifficultySelected == "EASY") )then
+        print("Add & Easy")
+    elseif( (worldSelected == "ADDITION") and
+            (levelDifficultySelected == "MEDIUM") )then
+            print("Add & Medium")
+    elseif( (worldSelected == "ADDITION") and
+           (levelDifficultySelected == "HARD") )then
+            print("Add & Hard")
+        
+    --subtraction world
+    elseif( (worldSelected == "SUBTRACTION") and
+        (levelDifficultySelected == "EASY") )then
+        print("Sub & Easy")
+    elseif( (worldSelected == "SUBTRACTION") and
+            (levelDifficultySelected == "MEDIUM") )then
+            print("Sub & Medium")
+    elseif( (worldSelected == "SUBTRACTION") and
+           (levelDifficultySelected == "HARD") )then
+            print("Sub & Hard")
+        
+    --multiplication world
+    elseif( (worldSelected == "MULTIPLICATION") and
+        (levelDifficultySelected == "EASY") )then
+        print("Multi & Easy")
+    elseif( (worldSelected == "MULTIPLICATION") and
+            (levelDifficultySelected == "MEDIUM") )then
+            print("Multi & Medium")
+    elseif( (worldSelected == "MULTIPLICATION") and
+           (levelDifficultySelected == "HARD") )then
+            print("Multi & Hard")
+        
+    --division world
+    elseif( (worldSelected == "DIVISION") and
+        (levelDifficultySelected == "EASY") )then
+        print("Div & Easy")
+    elseif( (worldSelected == "DIVISION") and
+            (levelDifficultySelected == "MEDIUM") )then
+            print("Div & Medium")
+    elseif( (worldSelected == "DIVISION") and
+           (levelDifficultySelected == "HARD") )then
+            print("Div & Hard")
+        
+    --sugar mountain world
+    elseif( (worldSelected == "SUGAR MOUNTAIN") and
+        (levelDifficultySelected == "EASY") )then
+        print("Sugar & Easy")
+    elseif( (worldSelected == "SUGAR MOUNTAIN") and
+            (levelDifficultySelected == "MEDIUM") )then
+            print("Sugar & Medium")
+    elseif( (worldSelected == "SUGAR MOUNTAIN") and
+           (levelDifficultySelected == "HARD") )then
+            print("Sugar & Hard")
+    end
 end
 
 function MainGameScene:draw()
     -- Codea does not automatically call this method
     background(0, 0, 0, 255)
     
-    --test to see if the worldSelected and levelDifficultySelected variables work for later if statements in the main game
+    --same test as above but in the draw function. *The equations will be done in here*
     fill(255, 255, 255, 255)
-    fontSize(50)
-    text("World: "..worldSelected, WIDTH/2, HEIGHT/2+300)
-    text("Difficulty: "..levelDifficultySelected, WIDTH/2, HEIGHT/2+200)
+    fontSize(60)
+    if( (worldSelected == "ADDITION") and
+        (levelDifficultySelected == "EASY") )then
+        text("Add & Easy", WIDTH/2, HEIGHT/2)
+    elseif( (worldSelected == "ADDITION") and
+            (levelDifficultySelected == "MEDIUM") )then
+            text("Add & Medium", WIDTH/2, HEIGHT/2)
+    elseif( (worldSelected == "ADDITION") and
+           (levelDifficultySelected == "HARD") )then
+            text("Add & Hard", WIDTH/2, HEIGHT/2)
+    end
 end
 
 function MainGameScene:touched(touch)
